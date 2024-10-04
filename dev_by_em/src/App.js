2b39dfbd7a3af123be06241e3258d5379867480a
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-10-04 16:17:40 +0100

    added responsive navbar and styled hero section

--- dev_by_em/src/App.js
+++ dev_by_em/src/App.js
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Hero />
       <About />
     </>
   );
